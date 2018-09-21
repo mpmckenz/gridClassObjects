059b719132df00aa952af7bff59a64e9b2698660
Michael McKenzie <mikemckenzie987@gmail.com>
2018-09-21 02:03:18 -0400

    done

---
+++ gridObject.js
@@ -0,0 +1,15 @@
+const grid = new Grid({
+    numberOfRows: 10,
+    numberOfColumns: 10,
+    parentContainerId: "grid",
+})
+const grid1 = new Grid({
+    numberOfRows: 4,
+    numberOfColumns: 4,
+    parentContainerId: "grid",
+})
+const grid2 = new Grid({
+    numberOfRows: 7,
+    numberOfColumns: 7,
+    parentContainerId: "grid",
+})
---
+++ script.js
@@ -0,0 +1,61 @@
+class Grid {
+    constructor(options) {
+        this.gridArray = []
+        this.numberOfRows = options.numberOfRows || 4
+        this.numberOfColumns = options.numberOfColumns || 5
+        this.gridContainer = document.getElementById(options.parentContainerId)
+        this.elementId = options.elementId
+        this.createRows()
+        this.createGridElement()
+    }
+
+    createGridElement() {
+        this.element = document.createElement("div")
+        this.element.id = this.elementId
+    }
+
+    createRows() {
+        for (let rowIndex = 0; rowIndex < this.numberOfRows; rowIndex++) {
+            this.rowIndex = rowIndex
+            this.rowArray = []
+            this.gridArray.push(this.rowArray)
+            this.rowElement = document.createElement("section")
+            this.gridContainer.appendChild(this.rowElement)
+            // this.rowElement.classList = rowIndex
+            for (let columnIndex = 0; columnIndex < this.numberOfColumns; columnIndex++) {
+                const cell = new Cell(rowIndex, columnIndex, this.rowElement)
+                this.rowArray.push(cell)
+            }
+        }
+        console.log(this.gridArray)
+    }
+    // getNeighborCellOf(cell) {
+    //     const cellRow = cell.rowIndex
+    //     const cellcolumn = cell.columnIndex
+    //     const neighbors = []
+    //     for (let rowIndex = //something) {
+    //         for (let columnIndex = // something) {
+
+    //         }
+    //     }
+    // }
+    // return neighbors
+}
+
+class Cell {
+    constructor(rowIndex, columnIndex, rowParent) {
+        // this.cellArray = []
+        this.rowIndex = rowIndex
+        this.columnIndex = columnIndex
+        this.rowParent = rowParent
+        // this.id = `${rowIndex} , ${columnIndex}`
+        this.createCell()
+    }
+    createCell() {
+        this.cellElement = document.createElement("article")
+        // this.cellElement.dataset.rowIndex = this.rowIndex
+        // this.cellElement.dataset.columnIndex = this.columnIndex
+        // this.cellElement.id = this.columnIndex
+        this.rowParent.appendChild(this.cellElement)
+    }
+}
